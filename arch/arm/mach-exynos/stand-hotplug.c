03ab0cc9333bf1aa480d11d5d80b4924db049eeb
myfluxi <mvdw@arcor.de>
2012-03-26 21:57:55 +0200

    stand-hotplug: Disable secondary cpu auto-hotplug when screen is off
    Thanks to arighi for the original commit.

--- arch/arm/mach-exynos/stand-hotplug.c
+++ arch/arm/mach-exynos/stand-hotplug.c
@@ -28,6 +28,7 @@
 #include <linux/reboot.h>
 #include <linux/gpio.h>
 #include <linux/cpufreq.h>
+#include <linux/earlysuspend.h>
 
 /* tegrak second core */
 #include <linux/device.h>
@@ -133,8 +134,9 @@ struct cpu_hotplug_info {
 	pid_t tgid;
 };
 
-
 static DEFINE_PER_CPU(struct cpu_time_info, hotplug_cpu_time);
+
+static bool screen_off;
 
 /* mutex can be used since hotplug_timer does not run in
    timer(softirq) context but in process context */
@@ -204,11 +206,16 @@ static void hotplug_timer(struct work_struct *work)
 
 	mutex_lock(&hotplug_lock);
 
+	if (screen_off && !cpu_online(1)) {
+		printk(KERN_INFO "pm-hotplug: disable cpu auto-hotplug\n");
+		goto out;
+	}
+
 	/* tegrak second core  */
 	if (!hotplug_on) {
 		if (!second_core_on && cpu_online(1) == 1)
 			cpu_down(1);
-		goto off_hotplug;
+		goto out;
 	}
 
 	if (user_lock == 1)
@@ -273,7 +280,7 @@ static void hotplug_timer(struct work_struct *work)
 no_hotplug:
 	queue_delayed_work_on(0, hotplug_wq, &hotplug_work, hotpluging_rate);
 
-off_hotplug:
+out:
 	mutex_unlock(&hotplug_lock);
 }
 
@@ -320,6 +327,29 @@ static int hotplug_reboot_notifier_call(struct notifier_block *this,
 
 static struct notifier_block hotplug_reboot_notifier = {
 	.notifier_call = hotplug_reboot_notifier_call,
+};
+
+static void hotplug_early_suspend(struct early_suspend *handler)
+{
+	mutex_lock(&hotplug_lock);
+	screen_off = true;
+	mutex_unlock(&hotplug_lock);
+}
+
+static void hotplug_late_resume(struct early_suspend *handler)
+{
+	printk(KERN_INFO "pm-hotplug: enable cpu auto-hotplug\n");
+
+	mutex_lock(&hotplug_lock);
+	screen_off = false;
+	queue_delayed_work_on(0, hotplug_wq, &hotplug_work, hotpluging_rate);
+	mutex_unlock(&hotplug_lock);
+}
+
+static struct early_suspend hotplug_early_suspend_notifier = {
+	.suspend = hotplug_early_suspend,
+	.resume = hotplug_late_resume,
+	.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
 };
 
 /* tegrak second core sysfs */
@@ -467,6 +497,7 @@ static int __init exynos4_pm_hotplug_init(void)
 #endif
 	register_pm_notifier(&exynos4_pm_hotplug_notifier);
 	register_reboot_notifier(&hotplug_reboot_notifier);
+	register_early_suspend(&hotplug_early_suspend_notifier);
 
 	/* tegrak second core  register device */
 	ret = misc_register(&second_core_device);
