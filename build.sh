11588dcc598912657d75222f305f53ce619ba16b
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-04-14 15:50:53 +0200

    galaxys2: build cifs module

--- build.sh
+++ build.sh
@@ -62,7 +62,7 @@ if [ 0 = "${#targets[@]}" ] ; then
     targets=(galaxys2 i777 galaxynote)
 fi
 
-MODULES_galaxys2=("drivers/samsung/fm_si4709/Si4709_driver.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcmdhd/dhd.ko")
+MODULES_galaxys2=("drivers/samsung/fm_si4709/Si4709_driver.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcmdhd/dhd.ko" "fs/cifs/cifs.ko")
 MODULES_galaxynote=("drivers/samsung/fm_si4709/Si4709_driver.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcmdhd/dhd.ko")
 MODULES_i777=("drivers/scsi/scsi_wait_scan.ko" "drivers/net/wireless/bcmdhd/dhd.ko")
 
