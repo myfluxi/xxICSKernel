c3e77be61fd29b9abf6e6cdeaa30a7fff11ddccd
Andrew Dodd <atd7@cornell.edu>
2012-04-25 17:55:28 -0400

    cpufreq_conservative: Bump default frequency step up to 10 from 5
    Based on a better understanding of how the governor operates,
    this is a better choice on most Exynos devices.
    
    Change-Id: I8ed87e2b61f621138aa5a20951d8a70037e986a9
    Signed-off-by: Andrew Dodd <atd7@cornell.edu>

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -92,7 +92,7 @@ static struct dbs_tuners {
 	.down_threshold = DEF_FREQUENCY_DOWN_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
-	.freq_step = 5,
+	.freq_step = 10,
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
