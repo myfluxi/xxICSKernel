579aa35ec7b2cb15121f950608c5e9ae674eb6d6
myfluxi <mvdw@arcor.de>
2012-04-25 10:53:58 +0200

    Revert "exynos4: Disable SW downclock in cpuidle"
    This reverts commit c040c8f53f8ffa37caaebf898ffa2a7798316d9a.

--- arch/arm/mach-exynos/cpuidle-exynos4.c
+++ arch/arm/mach-exynos/cpuidle-exynos4.c
@@ -680,7 +680,6 @@ static int exynos4_enter_idle(struct cpuidle_device *dev,
 	local_irq_disable();
 	do_gettimeofday(&before);
 
-#if 0
 	if (use_clock_down == SW_CLK_DWN) {
 		/* USE SW Clock Down */
 		cpu = get_cpu();
@@ -720,7 +719,6 @@ static int exynos4_enter_idle(struct cpuidle_device *dev,
 
 		put_cpu();
 	} else
-#endif
 		cpu_do_idle();
 
 	do_gettimeofday(&after);
