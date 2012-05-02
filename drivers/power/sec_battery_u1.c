366105ff489f50bb32c47c1999e75824c64639bc
XpLoDWilD <xplodgui@gmail.com>
2012-05-02 17:43:50 +0200

    sec_battery: Raise charger voltages by 50mA
    Change-Id: I776c7f34e0669d18e12f16dc2605ae0635f0621d

--- drivers/power/sec_battery_u1.c
+++ drivers/power/sec_battery_u1.c
@@ -1435,15 +1435,15 @@ static int sec_bat_enable_charging_main(struct sec_bat_info *info, bool enable)
 		switch (info->cable_type) {
 		case CABLE_TYPE_USB:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-			val_chg_current.intval = 450;	/* mA */
+			val_chg_current.intval = 500;	/* mA */
 			break;
 		case CABLE_TYPE_AC:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-			val_chg_current.intval = 650;	/* mA */
+			val_chg_current.intval = 700;	/* mA */
 			break;
 		case CABLE_TYPE_MISC:
 			val_type.intval = POWER_SUPPLY_STATUS_CHARGING;
-			val_chg_current.intval = 450;	/* mA */
+			val_chg_current.intval = 500;	/* mA */
 			break;
 		default:
 			dev_err(info->dev, "%s: Invalid func use\n", __func__);
