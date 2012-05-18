6b021adf831a194a593d83d8f810fdf44e524026
Tommi Airikka <tommi@airikka.net>
2012-05-19 01:13:14 +0200

    [cpufreq] add smartassV2 governor
    This adds smartassV2 governor cherry picked from
    https://github.com/erasmux/n1-kernel.git with conflicts that are
    resolved.
    Added into n7000 and galaxys2 defconfigs.
    These defines are also changed from the original commit:
    DEFAULT_AWAKE_IDEAL_FREQ 972000
    DEFAULT_SLEEP_IDEAL_FREQ 432000
    DEFAULT_RAMP_UP_STEP 270000
    DEFAULT_RAMP_DOWN_STEP 270000
    
    (cherry picked from commit d68d62a84ae2768888376a76a1f0aebd8f0b9302)
    
    Conflicts:
    
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Change-Id: I6f3651033143740c7c5651162119575ac7cf72bf

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -380,6 +380,9 @@ extern struct cpufreq_governor cpufreq_gov_adaptive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
 #endif
 
 
